c3570afbc05ecf205ef3bb9a39cd59943e01a2eb
sebruque <42419637+sebruque@users.noreply.github.com>
2018-08-15 14:33:34 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,185 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(251, 250, 239);
+  
+  color(130, 111, 200);
+  fill(130, 111, 200);
+  noStroke();
+  ellipse(460, 300, 50, 50);
+  
+  color(111, 162, 118);
+  fill(111, 162, 118);
+  noStroke();
+  ellipse(560, 350, 70, 70);
+  
+  color(250);
+  fill(250);
+  stroke(82, 171, 213);
+  strokeWeight(1);
+  ellipse(171, 200, 85, 85);
+  
+  color(250);
+  fill(250);
+  stroke(241,241, 121);
+  strokeWeight(1);
+  ellipse(257, 200, 85, 85);
+  
+  color(250);
+  fill(250);
+  stroke(238, 124, 40);
+  strokeWeight(1);
+  ellipse(342, 200, 85, 85);
+  
+  color(250);
+  fill(250);
+  stroke(65);
+  strokeWeight(1);
+  ellipse(428, 200, 85, 85);
+  
+  color(251, 250, 239);
+  fill(251, 250, 239);
+  noStroke();
+  rect(50, 200, 500, 45);
+  
+  color(240, 202, 86);
+  fill(240, 202, 86);
+  noStroke();
+  triangle(505, 315, 553, 260, 570, 330);
+  
+  color(250);
+  fill(250);
+  stroke(65);
+  strokeWeight(1);
+  ellipse(209, 246, 90, 90);
+  
+  color(250);
+  fill(250);
+  stroke(65);
+  strokeWeight(1);
+  ellipse(300, 246, 90, 90);
+  
+  color(251, 250, 239);
+  fill(251, 250, 239);
+  noStroke();
+  rect(100, 246, 250, 46);
+  
+  color(65);
+  fill(65);
+  stroke(65);
+  strokeWeight(1);
+  line(50, 200, 550, 200);
+  line(220, 10, 100, 450);
+  line(380, 10, 500, 450);
+  line(70, 400, 150, 400);
+  line(80, 422, 155, 422);
+  
+  color(241, 240, 131);
+  fill(241, 240, 131);
+  noStroke();
+  ellipse(58, 450, 69, 69);
+  
+  color(234, 212, 173);
+  fill(234, 212, 173);
+  noStroke();
+  ellipse(58, 450, 55, 55); 
+  
+  color(24, 125, 191);
+  fill(24, 125, 191);
+  noStroke();
+  ellipse(50,450, 40, 40);
+  
+  color(65);
+  fill(65);
+  stroke(65);
+  strokeWeight(1);
+  line(75, 405, 5, 485);
+  line(95, 370, 5, 430);
+  line(300, 150, 300, 450);
+  line(400, 300, 550, 300);
+  line(450, 360, 550, 360);
+  strokeWeight(2);
+  line(390, 120, 559, 335);
+  
+  color(250);
+  fill(250);
+  stroke(65);
+  strokeWeight(1);
+  ellipse(345, 285, 85, 85);
+  
+  color(251, 250, 239);
+  fill(251, 250, 239);
+  noStroke();
+  rect(302, 288, 83, 53);
+  
+  color(250);
+  fill(250);
+  stroke(65);
+  strokeWeight(1);
+  ellipse(375, 340, 85, 85);
+  
+  color(251, 250, 239);
+  fill(251, 250, 239);
+  noStroke();
+  rect(334, 350, 83, 53);
+  
+  color(65);
+  fill(65);
+  stroke(65);
+  strokeWeight(1);
+  line(260, 360, 370, 210);
+  line(264, 362, 372, 215);
+  line(268, 364, 374, 220); 
+  
+  color(65);
+  fill(65);
+  stroke(65);
+  strokeWeight(1);
+  line(270, 275, 370, 148);
+  line(266, 270, 366, 146);
+  line(262, 268, 363, 145);
+  
+  color(212, 61, 23);
+  fill(212, 61, 23);
+  stroke(212, 61, 23);
+  strokeWeight(3);
+  line(50, 195, 290, 110);
+  
+  color(65);
+  fill(65);
+  stroke(65);
+  strokeWeight(3);
+  line(50, 160, 310, 95);
+  
+  color(141,137, 137);
+  fill(141,137, 137);
+  stroke(141,137, 137);
+  strokeWeight(9);
+  line(292, 210, 369, 144);
+  
+  color(65);
+  fill(65);
+  stroke(65);
+  strokeWeight(9);
+  line(365, 147, 380, 133);
+  
+  color(173, 47, 15);
+  fill(173, 47, 15);
+  stroke(173, 47, 15);
+  strokeWeight(9);
+  line(380, 133, 510, 18);
+  
+  color(235, 240, 240);
+  fill(235, 240, 240);
+  stroke(235, 240, 240);
+  strokeWeight(9);
+  line(295, 207, 245, 249);
+  
+  color(65);
+  fill(65);
+  stroke(65);
+  strokeWeight(9);
+  line(243, 248, 252, 241);
+}
